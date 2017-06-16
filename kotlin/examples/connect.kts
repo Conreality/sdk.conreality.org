437413957eeabdf77782fbf3e6cc9deee1ba6615
Arto Bendiken <arto@conreality.org>
2017-06-16 22:44:42 +0200

    Added a connect examples for Kotlin.

--- kotlin/examples/connect.kts
+++ kotlin/examples/connect.kts
@@ -1,3 +1,17 @@
-#!/usr/bin/env kscript
+#!/usr/bin/env kotlinc -script
 
-// TODO: use PgJDBC?
+import java.sql.DriverManager
+
+Class.forName("org.postgresql.Driver").newInstance()
+
+val dbName = System.getenv("USER") ?: "test"
+
+val connection = DriverManager.getConnection("jdbc:postgresql:" + dbName)
+
+val statement = connection.createStatement()
+
+val resultSet = statement.executeQuery("SELECT 42 AS answer")
+
+while (resultSet.next()) {
+  println(resultSet.getInt("answer"))
+}
