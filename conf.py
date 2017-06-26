55469ad43328021d2a78eb799c3366740794a831
Arto Bendiken <arto@conreality.org>
2017-06-26 13:27:11 +0200

    Ensured that Sphinx can find the Python SDK's packages.

--- conf.py
+++ conf.py
@@ -20,6 +20,8 @@ import shlex
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('.extensions'))
+sys.path.append(os.path.abspath('.imports/conreality.py/src'))
 
 # -- General configuration ------------------------------------------------
 
